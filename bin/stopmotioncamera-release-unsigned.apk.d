de05bf4c48baf9c000657d15e50497fd0108a956
Potter <Robin.Potter@experian.com>
2019-07-24 21:14:46 +0100

    a clean branch and some noew code

--- bin/stopmotioncamera-release-unsigned.apk.d
+++
@@ -1,3 +0,0 @@
-/home/deadmeat/Spod/StopmotionCamera/bin/stopmotioncamera-release-unsigned.apk : \
-/home/deadmeat/Spod/StopmotionCamera/bin/stopmotioncamera.ap_ \
-/home/deadmeat/Spod/StopmotionCamera/bin/classes.dex \
--- gen/robin/stopmotion/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package robin.stopmotion;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
